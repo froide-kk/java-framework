39de0f2637b1959a820b349b6e8d31feba41a043
Gouki Sueoka <g-sueoka@froide.co.jp>
2021-04-06 18:11:06 +0900

    chapter1のソースを追加

---
+++ src/chapters/ch1_1/src/test/java/jp/co/froide/javaframework/ch1_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.froide.javaframework.ch1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
