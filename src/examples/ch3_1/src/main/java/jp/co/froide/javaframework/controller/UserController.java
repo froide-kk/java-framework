7951eedf9cbea76ea27a3ea9e7caf16eebdc5266
Gouki Sueoka <g-sueoka@froide.co.jp>
2021-05-16 20:12:19 +0900

    ch3_2

--- src/examples/ch3_1/src/main/java/jp/co/froide/javaframework/controller/UserController.java
+++ src/examples/ch3_1/src/main/java/jp/co/froide/javaframework/controller/UserController.java
@@ -19,4 +19,29 @@ public class UserController {
         return userDao.selectAll();
     }
 
+    @PostMapping("users")
+    public List<User> addUser(@ModelAttribute UserForm form) {
+        User user = new User();
+        user.setName(form.getName());
+        user.setPassword(form.getPassword());
+        userDao.insert(user);
+        return userDao.selectAll();
+    }
+
+    @PutMapping("users/{id}")
+    public List<User> updateUser(@PathVariable("id") int id, @ModelAttribute UserForm form) {
+        User user = userDao.selectById(id);
+        user.setName(form.getName());
+        user.setPassword(form.getPassword());
+        userDao.update(user);
+        return userDao.selectAll();
+    }
+
+    @DeleteMapping("users/{id}")
+    public List<User> deleteUser(@PathVariable("id") int id) {
+        User user = userDao.selectById(id);
+        userDao.delete(user);
+        return userDao.selectAll();
+    }
+
 }
